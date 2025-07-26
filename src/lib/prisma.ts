5eb911a2e7b90729c76cd4179a216ea4bfc13e3a
Maxwell Moses <61381438+Alixefin@users.noreply.github.com>
2025-07-26 13:00:13 -0600

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,37 +1,16 @@
 
-
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
+let prisma: PrismaClient;
 
-function getPrismaClient(): PrismaClient {
-  if (process.env.NODE_ENV === 'production') {
-    return new PrismaClient();
-  } else {
-    if (!globalForPrisma.prisma) {
-      globalForPrisma.prisma = new PrismaClient();
-    }
-    return globalForPrisma.prisma;
+// ensuring the client is instantiated globally *once* per serverless instance's cold start.
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
   }
+  prisma = global.prisma;
 }
 
-
-export const prisma = getPrismaClient();
-
-
-// export const prisma = getPrismaClient().$extends({
-//   query: {
-//     $allModels: {
-//       async $allOperations({ model, operation, query, args }) {
-//         const start = performance.now();
-//         const result = await query(args);
-//         const end = performance.now();
-//         const time = end - start;
-//         console.log(`Query: ${model}.${operation} took ${time.toFixed(2)}ms`);
-//         return result;
-//       },
-//     },
-//   },
-// });
+export { prisma }; // Export as named export
