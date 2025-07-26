d4ecc7def8e2d83123f5bf2e91e07530546a87e1
alixefin <efinnife@gmail.com>
2025-07-26 14:44:57 -0400

    Another

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,37 @@
-import { PrismaClient } from '@prisma/client'; 
-const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
 
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
+import { PrismaClient } from '@prisma/client';
 
-  });
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
+function getPrismaClient(): PrismaClient {
+  if (process.env.NODE_ENV === 'production') {
+    return new PrismaClient();
+  } else {
+    if (!globalForPrisma.prisma) {
+      globalForPrisma.prisma = new PrismaClient();
+    }
+    return globalForPrisma.prisma;
+  }
 }
+
+
+export const prisma = getPrismaClient();
+
+
+// export const prisma = getPrismaClient().$extends({
+//   query: {
+//     $allModels: {
+//       async $allOperations({ model, operation, query, args }) {
+//         const start = performance.now();
+//         const result = await query(args);
+//         const end = performance.now();
+//         const time = end - start;
+//         console.log(`Query: ${model}.${operation} took ${time.toFixed(2)}ms`);
+//         return result;
+//       },
+//     },
+//   },
+// });
